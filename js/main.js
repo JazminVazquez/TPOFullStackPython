5bf6febd5f326912b8aa7a3fad78a52f3cc3ea47
JazminVazquez <jaznvazquez@gmail.com>
2021-11-01 23:03:24 -0300

    Ultimas correcciones

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    if(document.getElementById("form")){
+    if (document.getElementById("form")) {
         document.getElementById("form").addEventListener('submit', validarFormulario);
     }
 
 });
 
-function validarFormulario(evento){
+function validarFormulario(evento) {
 
     evento.preventDefault();
     var name = document.getElementById('name').value;
     var mail = document.getElementById('mail').value;
     var tel = document.getElementById('tel').value;
     var msg = document.getElementById('msg').value;
-    if((name.length == 0) || (mail.length == 0) || (tel.length == 0) || (msg.length == 0)){
+    if ((name.length == 0) || (mail.length == 0) || (tel.length == 0) || (msg.length == 0)) {
 
         alert('Hay campos vacios');
         return;
@@ -23,7 +23,7 @@ function validarFormulario(evento){
 
 }
 
-window.onload=function(){
+window.onload = function () {
 
     let animacion = document.getElementById('animado');
     animacion.style.animation = 'mover 1s ease-out';
